2a1c545173e0060359ba283cb83c44e645afa421
SUKESH.P <sukesh20200@gmail.com>
2026-08-21 10:20:26 +0530

    Progam 3 rdbms

--- solution.sql
+++ solution.sql
@@ -1,1 +1,17 @@
+create database acadamicDB;
 
+USE acadamicoв;
+
+CREATE TABLE Student (Stuent ID numeric()primary key, studentName varchar(50), DOB date, gender varchar(18), department ID
+
+alter table student
+
+ADD
+
+Email varchar(10),
+
+phonellumber numeric(30)
+
+);
+
+dest student
